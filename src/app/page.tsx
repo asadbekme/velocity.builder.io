0e18222bc3be91d1f79428075f0f9a76c83873a5
asadbekme <asadbekme2002@gmail.com>
2023-09-28 23:43:48 +0500

    logic: beautiful background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,47 @@
+"use client";
+
 import Image from "next/image";
 import { assets } from "@/utils/backgrounds";
+import { useEffect, useState } from "react";
+import { type Framework, frameworks } from "@/utils/frameworks";
+import { cn } from "@/utils/tailwind-merge";
 
 const Home = () => {
+  const [currentFramework, setCurrentFramework] = useState<Framework>(
+    frameworks[0]
+  );
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const rotateFrameworks = () => {
+      setCurrentFramework(frameworks[currentIndex]);
+      currentIndex = (currentIndex + 1) % frameworks.length;
+    };
+
+    const intervalId = setInterval(rotateFrameworks, 2000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <main>
+      {/* BACKGROUND COLOR */}
+      <div
+        className={cn(
+          "fixed inset-0 transition-color delay-100 duration-700 opacity-25",
+          {
+            "bg-purple-300": currentFramework === "qwik",
+            "bg-sky-300": currentFramework === "safari",
+            "bg-yellow-300": currentFramework === "chrome",
+            "bg-teal-300": currentFramework === "tailwind",
+            "bg-blue-300": currentFramework === "react",
+            "bg-green-300": currentFramework === "vue",
+            "bg-orange-400": currentFramework === "svelte",
+            "bg-red-300": currentFramework === "mobile",
+            "bg-neutral-300": currentFramework === "desktop",
+          }
+        )}
+      />
+      {/* GRADIENT */}
       <Image
         width={1200}
         height={1200}
@@ -12,6 +50,7 @@ const Home = () => {
         className="fixed inset-0 w-screen h-screen object-cover"
         src={assets.gradient}
       />
+      {/* GRID */}
       <div
         className="fixed inset-0 opacity-30"
         style={{
